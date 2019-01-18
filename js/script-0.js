7454b8fc6f39a35ba8bea4b9b2f01f9a99f09d92
NEUAI <aizhengpeng@yeah.net>
2019-01-18 15:43:01 +0800

    update script-0.js

--- js/script-0.js
+++ js/script-0.js
@@ -1,4 +1,4 @@
-function loadCommonElements() {
+function loadTopNavbar(activeId) {
     let topNavbarNav = document.getElementById("topNavbarNav");
     let links = [
         {
@@ -33,6 +33,45 @@ function loadCommonElements() {
             img.setAttribute("src", links[i].imgSrc);
             img.setAttribute("width", "24");
             link.appendChild(img);
+            link.innerHTML += " ";
+        }
+        link.innerHTML += links[i].innerHTML;
+        if (i === activeId) {
+            link.setAttribute("class", "nav-item nav-link active");
+        } else {
+            link.setAttribute("class", "nav-item nav-link");
+        }
+        link.setAttribute("href", links[i].href);
+        if (links[i].hasOwnProperty("target")) {
+            link.setAttribute("target", links[i].target);
+        }
+        topNavbarNav.appendChild(link);
+    }
+}
+
+function loadBottomNavbar() {
+    let bottomNavbarNav = document.getElementById("bottomNavbarNav");
+    let links = [
+        {
+            "innerHTML": "辽ICP备18008623号-1",
+            "href": "http://www.miitbeian.gov.cn/publish/query/indexFirst.action",
+            "target": "_blank"
+        }, {
+            "imgSrc": "/img/ghs.png",
+            "innerHTML": "辽公网安备21011202000299号",
+            "href": "http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=21011202000299",
+            "target": "_blank"
+        }
+    ];
+    for (let i = 0; i < links.length; i++) {
+        let link = document.createElement("a");
+        if (links[i].hasOwnProperty("imgSrc")) {
+            let img = document.createElement("img");
+            img.setAttribute("class", "d-inline-block align-top");
+            img.setAttribute("src", links[i].imgSrc);
+            img.setAttribute("width", "18");
+            link.appendChild(img);
+            link.innerHTML += " ";
         }
         link.innerHTML += links[i].innerHTML;
         link.setAttribute("class", "nav-item nav-link");
@@ -40,6 +79,11 @@ function loadCommonElements() {
         if (links[i].hasOwnProperty("target")) {
             link.setAttribute("target", links[i].target);
         }
-        topNavbarNav.appendChild(link);
+        bottomNavbarNav.appendChild(link);
     }
+}
+
+function loadCommonElements(activeId) {
+    loadTopNavbar(activeId);
+    loadBottomNavbar();
 }
